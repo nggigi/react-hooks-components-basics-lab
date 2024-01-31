98777be884a01034aa63a36e2369829f82a21939
nggigi <ngigi.francis5@gmail.com>
2024-01-31 08:35:49 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -15,16 +16,23 @@ function Home() {
     </div>
   );
 }
+function About(){
+  return(
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
-{/* write an <About> component here */}
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
